55a13b5e68d4f955ee8fd61550aaf358233a639c
Guczu <jakub180720@gmail.com>
2024-03-09 22:38:13 +0100

    fix(login,register): change images and login form

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
 import PrimaryInput from '../../components/PrimaryInput/PrimaryInput'
 import PrimaryButton from '../../components/PrimaryButton/PrimaryButton'
+import loginPhoto from '../../assets/login.webp'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <section className='w-screen h-full flex justify-center items-center gap-24'> 
+    <section className='w-screen h-full flex justify-center items-center'> 
         <img 
-          src="https://img.freepik.com/darmowe-wektory/ilustracja-koncepcja-konta_114360-399.jpg?w=826&t=st=1706805551~exp=1706806151~hmac=5bb59f24b903af0a37aee9b290efca04bffcce9c46e6f653d103ca14f1b877f0" 
+          src={loginPhoto}
           alt="login picture" 
-          className='w-[600px] lg:flex hidden'  
+          className='w-2/5 lg:flex hidden'  
         />
         <div className='w-full lg:w-min flex flex-col gap-6 bg-white p-12 rounded-xl z-50'>
             <span className='text-[42px] lg:text-[72px] font-medium'>Marketplace</span>
--- src/views/Register/Register.tsx
+++ src/views/Register/Register.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
 import PrimaryInput from '../../components/PrimaryInput/PrimaryInput'
 import PrimaryButton from '../../components/PrimaryButton/PrimaryButton'
+import registerPhoto from '../../assets/register.webp'
 
 const Register = () => {
   return (
-    <section className='w-screen h-full flex justify-center items-center gap-24'> 
+    <section className='w-screen h-full flex justify-center items-center'> 
         <img 
-          src="https://img.freepik.com/darmowe-wektory/wypelnij-ilustracje-koncepcyjna_114360-5450.jpg?w=826&t=st=1706805754~exp=1706806354~hmac=96ee53130919be30f61537624a685badbee73acd1d5981ea9cd816c391ec6387" 
+          src={registerPhoto}
           alt="register picture" 
-          className='w-[600px] lg:flex hidden'  
+          className='w-2/5 lg:flex hidden'  
         />
         <div className='w-full lg:w-min flex flex-col gap-4 bg-white p-12 rounded-xl'>
             <span className='text-[42px] lg:text-[72px] font-medium'>Marketplace</span>
+            <PrimaryInput id="username" label="Nazwa użytkownika" placeholder="Wpisz nazwę użytkownika" styles="flex flex-col" />
             <PrimaryInput id="email" label="E-mail" placeholder="Wpisz e-mail" styles="flex flex-col" />
             <PrimaryInput id="password" type="password" label="Hasło" placeholder="Hasło" styles="flex flex-col" />
             <PrimaryInput id="confirmpassword" type="password" label="Powtórz hasło" placeholder="Powtórz hasło" styles="flex flex-col" />
